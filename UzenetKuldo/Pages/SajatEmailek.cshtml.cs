52be0dde4f0bae74bdf837cd97ffe346fb8d8f45
Gerguraki555 <gergurakki@gmail.com>
2025-06-14 18:03:04 +0200

    Hozzáadtuk a saját email funkciót.

---
+++ UzenetKuldo/Pages/SajatEmailek.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UzenetKuldo.Pages
+{
+    public class SajatEmailekModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
